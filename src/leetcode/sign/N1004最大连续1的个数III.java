7c3145fec4252aea9dda9d9e8296d8a4c9516227
Ikarosx <532288425@qq.com>
2021-02-19 14:04:46 +0800

    20210219 签到

---
+++ src/leetcode/sign/N1004最大连续1的个数III.java
@@ -0,0 +1,62 @@
+package leetcode.sign;
+
+/**
+ * 给定一个由若干 0 和 1 组成的数组 A，我们最多可以将 K 个值从 0 变成 1 。
+ * <p>
+ * 返回仅包含 1 的最长（连续）子数组的长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：A = [1,1,1,0,0,0,1,1,1,1,0], K = 2
+ * 输出：6
+ * 解释：
+ * [1,1,1,0,0,1,1,1,1,1,1]
+ * 粗体数字从 0 翻转到 1，最长的子数组长度为 6。
+ * 示例 2：
+ * <p>
+ * 输入：A = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], K = 3
+ * 输出：10
+ * 解释：
+ * [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+ * 粗体数字从 0 翻转到 1，最长的子数组长度为 10。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= A.length <= 20000
+ * 0 <= K <= A.length
+ * A[i] 为 0 或 1 
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/max-consecutive-ones-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Ikarosx
+ * @date 2021/2/19 12:49
+ */
+public class N1004最大连续1的个数III {
+
+
+    public int longestOnes(int[] A, int K) {
+        int N = A.length;
+        int res = 0;
+        int left = 0, right = 0;
+        int zeros = 0;
+        while (right < N) {
+            if (A[right] == 0) {
+                zeros++;
+            }
+            while (zeros > K) {
+                if (A[left++] == 0) {
+                    zeros--;
+                }
+            }
+            res = Math.max(res, right - left + 1);
+            right++;
+        }
+        return res;
+    }
+
+}
