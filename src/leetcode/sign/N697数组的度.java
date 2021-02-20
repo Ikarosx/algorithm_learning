12e7ef6dbe30067797aedff31cec7fd90a915321
Ikarosx <532288425@qq.com>
2021-02-20 13:54:28 +0800

    20210220 签到

---
+++ src/leetcode/sign/N697数组的度.java
@@ -0,0 +1,61 @@
+package leetcode.sign;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个非空且只包含非负数的整数数组 nums，数组的度的定义是指数组里任一元素出现频数的最大值。
+ * <p>
+ * 你的任务是在 nums 中找到与 nums 拥有相同大小的度的最短连续子数组，返回其长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[1, 2, 2, 3, 1]
+ * 输出：2
+ * 解释：
+ * 输入数组的度是2，因为元素1和2的出现频数最大，均为2.
+ * 连续子数组里面拥有相同度的有如下所示:
+ * [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+ * 最短连续子数组[2, 2]的长度为2，所以返回2.
+ * 示例 2：
+ * <p>
+ * 输入：[1,2,2,3,1,4,2]
+ * 输出：6
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/degree-of-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Ikarosx
+ * @date 2021/2/20 10:50
+ */
+public class N697数组的度 {
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer, int[]> map = new HashMap<Integer, int[]>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+                map.get(nums[i])[0]++;
+                map.get(nums[i])[2] = i;
+            } else {
+                map.put(nums[i], new int[]{1, i, i});
+            }
+        }
+        int maxNum = 0, minLen = 0;
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            int[] arr = entry.getValue();
+            if (maxNum < arr[0]) {
+                maxNum = arr[0];
+                minLen = arr[2] - arr[1] + 1;
+            } else if (maxNum == arr[0]) {
+                if (minLen > arr[2] - arr[1] + 1) {
+                    minLen = arr[2] - arr[1] + 1;
+                }
+            }
+        }
+        return minLen;
+    }
+
+}
