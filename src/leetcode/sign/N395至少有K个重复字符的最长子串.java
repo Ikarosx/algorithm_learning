82225255c944787796999902d24633b415fcc1c6
Ikarosx <532288425@qq.com>
2021-02-28 00:19:20 +0800

    202102212728 签到

---
+++ src/leetcode/sign/N395至少有K个重复字符的最长子串.java
@@ -0,0 +1,38 @@
+package leetcode.sign;
+
+/**
+ * 给你一个字符串 s 和一个整数 k ，请你找出 s 中的最长子串， 要求该子串中的每一字符出现次数都不少于 k 。返回这一子串的长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "aaabb", k = 3
+ * 输出：3
+ * 解释：最长子串为 "aaa" ，其中 'a' 重复了 3 次。
+ * 示例 2：
+ * <p>
+ * 输入：s = "ababbc", k = 2
+ * 输出：5
+ * 解释：最长子串为 "ababb" ，其中 'a' 重复了 2 次， 'b' 重复了 3 次。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 仅由小写英文字母组成
+ * 1 <= k <= 105
+ * 通过次数34,189提交次数67,502
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-with-at-least-k-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Ikarosx
+ * @date 2021/02/27
+ */
+public class N395至少有K个重复字符的最长子串 {
+    public int longestSubstring(String s, int k) {
+        return 0;
+    }
+}
