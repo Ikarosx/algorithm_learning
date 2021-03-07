1861c5c3c3490bcebed34ae9c5e0a6669ec5414e
Ikarosx <532288425@qq.com>
2021-03-07 23:44:17 +0800

    20210307 签到

---
+++ src/leetcode/sign/N131分割回文串.java
@@ -0,0 +1,72 @@
+package leetcode.sign;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
+ *
+ * 回文串 是正着读和反着读都一样的字符串。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "aab"
+ * 输出：[["a","a","b"],["aa","b"]]
+ * 示例 2：
+ *
+ * 输入：s = "a"
+ * 输出：[["a"]]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 16
+ * s 仅由小写英文字母组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-partitioning
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Ikarosx
+ * @date 2021/03/07
+ */
+public class N131分割回文串 {
+    boolean[][] f;
+    List<List<String>> ret = new ArrayList<List<String>>();
+    List<String> ans = new ArrayList<String>();
+    int n;
+
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        f = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(f[i], true);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+            }
+        }
+
+        dfs(s, 0);
+        return ret;
+    }
+
+    public void dfs(String s, int i) {
+        if (i == n) {
+            ret.add(new ArrayList<String>(ans));
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (f[i][j]) {
+                ans.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+}
