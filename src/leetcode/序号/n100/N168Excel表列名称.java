873208b71005b1d1d3ceb0ef187745a22b5c8b30
许培宇 <532288425@qq.com>
2021-04-26 10:59:00 +0800

    N168Excel表列名称 进制转换

--- src/leetcode/序号/n100/N168Excel表列名称.java
+++ src/leetcode/序号/n100/N168Excel表列名称.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 
 /**
  * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
- *
+ * <p>
  * 例如，
- *
+ * <p>
  * 1 -> A
  * 2 -> B
  * 3 -> C
@@ -16,28 +16,32 @@ import org.junit.Test;
  * 27 -> AA
  * 28 -> AB
  * ...
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: 1
  * 输出: "A"
- * 示例 2:
- *
+ * <p>
+ * 示例&nbsp;2:
+ * <p>
  * 输入: 28
  * 输出: "AB"
- * 示例 3:
- *
+ * <p>
+ * 示例&nbsp;3:
+ * <p>
  * 输入: 701
  * 输出: "ZY"
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/excel-sheet-column-title
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * <p>
+ * 相关标签：
+ * 数学
+ * <p>
+ * 原题地址：https://leetcode-cn.com/problems/excel-sheet-column-title/
  *
  * @author Ikarosx
- * @date 2021/1/4 13:06
+ * @date 2021/4/26
  */
 public class N168Excel表列名称 {
-
     @Test
     public void test() {
         Assert.assertEquals("A", convertToTitle(1));
@@ -45,13 +49,18 @@ public class N168Excel表列名称 {
         Assert.assertEquals("ZY", convertToTitle(701));
     }
 
-    public String convertToTitle(int n) {
-        StringBuilder builder = new StringBuilder();
-        while (n != 0) {
-            n--;
-            builder.insert(0, (char) (n % 26 + 'A'));
-            n /= 26;
+    /**
+     * 本质上是进制转换
+     *
+     * @param columnNumber
+     * @return
+     */
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while (columnNumber > 0) {
+            sb.insert(0, (char) ((columnNumber - 1) % 26 + 'A'));
+            columnNumber = (columnNumber - 1) / 26;
         }
-        return builder.toString();
+        return sb.toString();
     }
 }
