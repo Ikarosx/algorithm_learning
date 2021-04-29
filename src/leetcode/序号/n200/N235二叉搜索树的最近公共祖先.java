5374af16c1a3da77eab58b7f24289c01ca8f7fbb
许培宇 <532288425@qq.com>
2021-04-29 12:22:50 +0800

    N235二叉搜索树的最近公共祖先 递归

--- src/leetcode/序号/n200/N235二叉搜索树的最近公共祖先.java
+++ src/leetcode/序号/n200/N235二叉搜索树的最近公共祖先.java
@@ -2,48 +2,131 @@ package leetcode.序号.n200;
 
 import 剑指offer.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * <a href="https://baike.baidu.com/item/%E6%9C%80%E8%BF%91%E5%85%AC%E5%85%B1%E7%A5%96%E5%85%88/8918834?fr=aladdin">百度百科</a>中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉搜索树:&nbsp; root =&nbsp;[6,2,8,0,4,7,9,null,null,3,5]
+ *
+ * <img style="height: 190px; width: 200px;" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/14/binarysearchtree_improved.png" alt="">
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ * <p>
+ * 说明:
+ * <p>
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ * <p>
+ * <p>
+ * 相关标签：
+ * 树
+ * <p>
+ * 原题地址：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ *
+ * @author Ikarosx
+ * @date 2021/4/27
+ */
+public class N235二叉搜索树的最近公共祖先 {
+    /**
+     * 遍历计数
+     */
+    public TreeNode lowestCommonAncestorFor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q) {
+            // 如果结点是其中一个，则直接返回
+            return root;
+        }
+        // 计算左子树有多少个值
+        int leftCount = count(root.left, p, q);
+        // 左右各一个
+        if (leftCount == 1) {
+            return root;
+        }
+        // 左边0右边2
+        if (leftCount == 0) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+    }
 
-<a href="https://baike.baidu.com/item/%E6%9C%80%E8%BF%91%E5%85%AC%E5%85%B1%E7%A5%96%E5%85%88/8918834?fr=aladdin">百度百科</a>中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+    private int count(TreeNode left, TreeNode p, TreeNode q) {
+        if (left == null) {
+            return 0;
+        }
+        if (left == p || left == q) {
+            return 1 + count(left.left, p, q) + count(left.right, p, q);
+        }
+        return count(left.left, p, q) + count(left.right, p, q);
+    }
 
-例如，给定如下二叉搜索树:&nbsp; root =&nbsp;[6,2,8,0,4,7,9,null,null,3,5]
+    /**
+     * 两次遍历
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path_p = getPath(root, p);
+        List<TreeNode> path_q = getPath(root, q);
+        TreeNode ancestor = null;
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            if (path_p.get(i) == path_q.get(i)) {
+                ancestor = path_p.get(i);
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
 
-<img style="height: 190px; width: 200px;" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/14/binarysearchtree_improved.png" alt="">
+    /**
+     * 获取从根节点到目标结点的路径
+     *
+     * @param root
+     * @param target
+     * @return
+     */
+    public List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new ArrayList<TreeNode>();
+        TreeNode node = root;
+        while (node != target) {
+            path.add(node);
+            if (target.val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        path.add(node);
+        return path;
+    }
 
-示例 1:
+	/**
+	 * 一次遍历
+	 */
+    public TreeNode lowestCommonAncestorOne(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
 
-输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
-输出: 6 
-解释: 节点 2 和节点 8 的最近公共祖先是 6。
-
-示例 2:
-
-输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
-输出: 2
-解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
-
-说明:
-
-	所有节点的值都是唯一的。
-	p、q 为不同节点且均存在于给定的二叉搜索树中。
-
-
-相关标签：
-	树
-
-原题地址：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
-  
- *  
- * @author Ikarosx 
- * @date 2021/4/27 
- */ 
- public class N235二叉搜索树的最近公共祖先 { 
- 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		if (root == p || root == q) {
-			// 如果结点是其中一个，则直接返回
-			return root;
-		}
-
-	}
- } 
+}
